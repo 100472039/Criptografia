77dc91badf02d6dcb40f73b07a9ad3da9f776947
ague17 <aguedi17@gmail.com>
2023-10-18 17:26:11 +0200

    versión 1 de kdf

---
+++ kdf.py
@@ -0,0 +1,47 @@
+import os 
+#from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+#eso no lo necesitamos todavía
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+def derivar (password):
+
+    #salt generada aleatoriamente
+    salt = os.urandom(16)
+
+    #derive
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
+    key= kdf.derive(password)
+    return key, salt;
+
+def verificar(key, salt, newpassword):
+    # verify
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
+
+    try: out=kdf.verify(newpassword, key) 
+    except: print("Password is not correct")
+    else: 
+        if out==None:
+            print("Password is correct")
+        else:
+            print("qué mierdas es esto?")
+
+#contraseña buena
+password = b"hola"
+
+#preguntar password
+newpassword = input("Introduzca su contraseña: ")
+newpassword = bytes(newpassword, encoding='utf-8')
+
+key, salt = derivar(password)
+verificar(key, salt, newpassword)
