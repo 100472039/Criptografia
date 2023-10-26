a1397f2cd42b6249895c196ea9eb6a458d86bfbb
ague17 <aguedi17@gmail.com>
2023-10-26 22:17:46 +0200

    añadir etiquetas de autenticaciçon de mensaje

--- asimetrico.py
+++ asimetrico.py
@@ -3,14 +3,16 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hmac
+from cryptography.hazmat.backends import default_backend
 
 
-# Creando claves asimétricas
-privada = rsa.generate_private_key(public_exponent=65537,key_size=2048)
-publica=privada.public_key()
+# Generar claves asimétricas
+privada = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+publica = privada.public_key()
 
 
-#generar clave simétrica 
+# Generar clave simétrica
 simetrica = Fernet.generate_key()
 
 # Mandar asimétricamente la clave simétrica
@@ -25,6 +27,7 @@ def cifrar_con_publica(publica, mensaje):
     )
     return cifrado
 
+
 def descifrar_con_privada(cifrado, privada):
     mensaje = privada.decrypt(
         cifrado,
@@ -36,20 +39,43 @@ def descifrar_con_privada(cifrado, privada):
     )
     return mensaje
 
-# Cifrar simétricamente los datos
-def cifrado_simetrico(simetrica, mensaje):
-    cipher_suite = Fernet(simetrica)
-    cifrado = cipher_suite.encrypt(mensaje)
-    print(f'Mensaje cifrado simétricamente: {cifrado}')
-    return cifrado
 
-def descifrado_simetrico(simetrica, cifrado):
-    cipher_suite = Fernet(simetrica)
+# Cifrar simétricamente los datos con etiqueta de autenticación
+def cifrado_simetrico(simetrica, mensaje):
+    f = Fernet(simetrica)
+    cifrado = f.encrypt(mensaje)
+
+    h = hmac.HMAC(simetrica, hashes.SHA256(), backend=default_backend())
+    h.update(cifrado)
+    tag = h.finalize()
+
+    print(f'Mensaje cifrado simétricamente: {cifrado}')
+    print(f'Etiqueta de autenticación: {tag}')
+    return cifrado, tag
+
+
+# Descifrar simétricamente los datos y verificar la etiqueta de autenticación
+def descifrado_simetrico(simetrica, cifrado, tag):
+
     try:
-        mensaje = cipher_suite.decrypt(cifrado)
+        h = hmac.HMAC(simetrica, hashes.SHA256(), backend=default_backend())
+        h.update(cifrado)
+        h.verify(tag)
+
+        f = Fernet(simetrica)
+        mensaje = f.decrypt(cifrado)
         print(f'Mensaje descifrado simétricamente: {mensaje}')
         return mensaje
-    except Exception as e:
-        print(f'Error al descifrar el mensaje: {e}')
+
+    except Exception:
+        print(f'Error al descifrar el mensaje o verificar la autenticidad: {Exception}')
         return None
+
+
+# Ejemplo de uso
+mensaje_original = b"Este es un mensaje secreto"
+clave_asimetrica = cifrar_con_publica(publica, simetrica)
+cifrado, tag = cifrado_simetrico(simetrica, mensaje_original)
+descifrado_simetrico(simetrica, cifrado, tag)
+
     
